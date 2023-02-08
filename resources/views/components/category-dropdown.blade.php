55d9334ca67aab21324c8b4d9bb6fcb7da34ef22
Isidro Moran <issildur@gmail.com>
2023-02-08 15:36:26 -0500

    Lesson 49 Simple Pagination

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -6,11 +6,17 @@
         </button>
     </x-slot>
 
-    <x-dropdown-item href="/" :active="request()->routeIs('home')">All</x-dropdown-item>
-
+    <x-dropdown-item href="/?{{ http_build_query(request()->except('category',
+                                                                    'page')) }}" 
+                    :active="request()->routeIs('home')">
+            All
+    </x-dropdown-item>
+ @php
+     
+ @endphp
     @foreach ($categories as $category)
         <x-dropdown-item 
-            href="/?category={{ $category->slug }}"
+            href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category','page')) }}"
             :active='request()->is("categories/{{ $category->slug }}")'>
                 {{ ucwords($category->name) }}
         </x-dropdown-item>
